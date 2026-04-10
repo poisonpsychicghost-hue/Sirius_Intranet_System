1fb505c626fcc675cb72ac6bab8a8841313f0b17
Caleb Barnard <poisonpsychicghost@gmail.com>
2026-04-10 18:23:06 -0400

    more work on server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const fs = require('fs');
+const DATA_FILE = DataTransfer.json;
 
 app.use(express.json());
 
@@ -12,3 +14,33 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log('Server started on https:localhost:${PORT}');
 });
+
+//helper: Read All Memos from data.json
+function readMemos() {
+    try {
+        const data = fs.readFileSync(DATA_FILE, "utf-8");
+        return JSON.parse(data);
+    } catch (err) {
+        //if missing return blank list
+        return [];
+    }
+}
+
+//Helper: Write memos to data.json
+function writeMemos(memos) {
+    fs.writeFileSync(DATA_FILE, JSON.stringify(memos, null, 2));  
+}
+
+//Get all Memos
+app.get('/memos', (req, res) => {
+    const memos = readMemos();
+    res.json(memos)
+});
+
+//Post new Memos
+app.post('/memos'), (req, res) => {
+    const memos = readMemos();
+    memos.push(req.body.memo);
+    writeMemos(memos);
+    res.json({status: 'ok' });
+};
